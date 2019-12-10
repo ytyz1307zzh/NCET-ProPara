6f9576d1a30052d3bb543fa2597c1b59208ee87c
Zhihan Zhang <zhangzhihan@pku.edu.cn>
2019-12-10 22:43:25 +0800

    change the output of collate_fn: previously the output format is identical to the input dict, now the output is a dict containing batched tensors of data fields

--- Dataset.py
+++ Dataset.py
@@ -53,8 +53,6 @@ class ProparaDataset(torch.utils.data.Dataset):
         total_loc_cands = instance['total_loc_candidates']
         loc_cand_list = instance['loc_cand_list']
 
-        print('total_loc_cands: ', total_loc_cands)
-
         metadata = {'para_id': para_id,
                     'entity': entity_name,
                     'total_sents': total_sents,
@@ -96,56 +94,98 @@ class ProparaDataset(torch.utils.data.Dataset):
 
 class Collate:
     """
-    a variant of callate_fn that pads according to the longest sequence in
-    a batch of sequences
+    A variant of callate_fn that pads according to the longest sequence in
+    a batch of sequences, turn List[Dict] -> Dict[List]
     """
+    def __init__(self):
+        pass
 
-    def collate(self, batch: List[Dict]) -> List[Dict]:
+
+    def __call__(self, batch):
+        return self.collate(batch)
+
+
+    def collate(self, batch: List[Dict]):
         """
+        Convert a list of dict instances to a dict of batched tensors
+
         args:
             batch - list of instances constructed by dataset
 
         reutrn:
-            batch - list of padded instances
+            batch - a dict, contains lists of data fields
         """
         # find max number of sentences & tokens
         max_sents = max([inst['metadata']['total_sents'] for inst in batch])
         max_tokens = max([len(inst['paragraph']) for inst in batch])
-        print('max_sents: ', max_sents)
-        print('max_tokens: ', max_tokens)
+        max_cands = max([inst['metadata']['total_loc_cands'] for inst in batch])
+        batch_size = len(batch)
 
         # pad according to max_len
-        batch = list(map(lambda x: self.pad_instance(x, max_sents = max_sents, max_tokens = max_tokens), batch))
-        return batch
+        batch = list(map(lambda x: self.pad_instance(x, max_sents = max_sents, 
+                                                        max_tokens = max_tokens, 
+                                                        max_cands = max_cands), batch))
+
+        metadata = list(map(lambda x: x['metadata'], batch))
+        paragraph = list(map(lambda x: x['paragraph'], batch))
+        gold_loc_seq = torch.stack(list(map(lambda x: x['gold_loc_seq'], batch)))
+        gold_state_seq = torch.stack(list(map(lambda x: x['gold_state_seq'], batch)))
+        entity_mask = torch.stack(list(map(lambda x: x['entity_mask'], batch)))
+        verb_mask = torch.stack(list(map(lambda x: x['verb_mask'], batch)))
+        loc_mask = torch.stack(list(map(lambda x: x['loc_mask'], batch)))
+
+        # check the dimension of the data
+        assert len(metadata) == len(paragraph) == batch_size
+        assert gold_loc_seq.size() == gold_state_seq.size() == (batch_size, max_sents)
+        assert entity_mask.size() == verb_mask.size() == (batch_size, max_sents, max_tokens)
+        assert loc_mask.size() == (batch_size, max_cands, max_sents, max_tokens)
+
+        return {'metadata': metadata,
+                'paragraph': paragraph,
+                'gold_loc_seq': gold_loc_seq,
+                'gold_state_seq': gold_state_seq,
+                'entity_mask': entity_mask,
+                'verb_mask': verb_mask,
+                'loc_mask': loc_mask
+                }
 
     
-    def pad_instance(self, instance: Dict, max_sents: int, max_tokens: int) -> Dict:
+    def pad_instance(self, instance: Dict, max_sents: int, max_tokens: int, max_cands: int) -> Dict:
         """
-        args: instance - instance to pad
-        max_sents: maximum number of sentences in this batch
-        max_tokens: maximum number of tokens in this batch
+        Pad the data fields of a certain instance.
+
+        args: 
+            instance - instance to pad
+            max_sents -  maximum number of sentences in this batch
+            max_tokens -  maximum number of tokens in this batch
         """
         instance['gold_state_seq'] = self.pad_tensor(instance['gold_state_seq'], pad = max_sents, dim = 0)
         instance['gold_loc_seq'] = self.pad_tensor(instance['gold_loc_seq'], pad = max_sents, dim = 0, pad_val = PAD_LOC)
         
         instance['entity_mask'] = self.pad_mask_list(instance['entity_mask'], max_sents = max_sents, max_tokens = max_tokens)
         instance['verb_mask'] = self.pad_mask_list(instance['verb_mask'], max_sents = max_sents, max_tokens = max_tokens)
-        instance['loc_mask'] = torch.stack(list(map(lambda x: self.pad_mask_list(x, max_sents = max_sents, max_tokens = max_tokens), 
-                                        instance['loc_mask'])))
+        instance['loc_mask'] = self.pad_mask_list(instance['loc_mask'], max_sents = max_sents,
+                                                     max_tokens = max_tokens, max_cands = max_cands)
 
         return instance
 
     
-    def pad_mask_list(self, vec: torch.Tensor, max_sents: int, max_tokens: int) -> torch.Tensor:
+    def pad_mask_list(self, vec: torch.Tensor, max_sents: int, max_tokens: int, max_cands: int = None) -> torch.Tensor:
         """
         Pad a tensor of mask list
         """
-        tmp_vec = self.pad_tensor(vec, pad = max_tokens, dim = 1)
-        return self.pad_tensor(tmp_vec, pad = max_sents, dim = 0)
+        tmp_vec = self.pad_tensor(vec, pad = max_tokens, dim = -1)
+        tmp_vec = self.pad_tensor(tmp_vec, pad = max_sents, dim = -2)
+        if max_cands is not None:
+            tmp_vec = self.pad_tensor(tmp_vec, pad = max_cands, dim = -3)
+
+        return tmp_vec
 
     
     def pad_tensor(self, vec: torch.Tensor, pad: int, dim: int, pad_val: int = 0) -> torch.Tensor:
         """
+        Pad a tensor on a given dimension to a given size.
+
         args:
             vec - tensor to pad
             pad - the size to pad to
@@ -156,15 +196,11 @@ class Collate:
         """
         pad_size = list(vec.size())
         pad_size[dim] = pad - vec.size(dim)
-        pad_vec = torch.zeros(*pad_size, dtype=vec.dtype)
+        pad_vec = torch.zeros(*pad_size, dtype = vec.dtype)
 
         if pad_val != 0:
             pad_vec.fill_(pad_val)
 
-        return torch.cat([vec, pad_vec], dim=dim)
-
-
-    def __call__(self, batch):
-        return self.collate(batch)
+        return torch.cat([vec, pad_vec], dim = dim)
 
         
