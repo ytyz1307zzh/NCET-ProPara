fix the issue of reporting loss and accuracy, now these scores are averaged over all instances

Model.py
@@ -54,14 +54,14 @@ class NCETModel(nn.Module):
         # size (batch, max_sents, NUM_STATES)
         tag_logits = self.StateTracker(encoder_out = token_rep, entity_mask = entity_mask, verb_mask = verb_mask)
         tag_mask = (gold_state_seq != PAD_STATE) # mask the padded part so they won't count in loss
-        log_likelihood = self.CRFLayer(emissions = tag_logits, tags = gold_state_seq.long(), mask = tag_mask, reduction = 'mean')
+        log_likelihood = self.CRFLayer(emissions = tag_logits, tags = gold_state_seq.long(), mask = tag_mask, reduction = 'sum')
 
         loss = -log_likelihood  # State classification loss is negative log likelihood
         pred_sequence = self.CRFLayer.decode(emissions = tag_logits, mask = tag_mask)
         assert len(pred_sequence) == batch_size
-        accuracy = compute_tag_accuracy(pred = pred_sequence, gold = gold_state_seq.tolist(), pad_value = PAD_STATE)
+        correct_pred, total_pred = compute_tag_accuracy(pred = pred_sequence, gold = gold_state_seq.tolist(), pad_value = PAD_STATE)
 
-        return loss, accuracy
+        return loss, correct_pred, total_pred
 
     
 class NCETEmbedding(nn.Module):

train.py
@@ -103,8 +103,8 @@ def train():
 
         model.train()
         train_instances = len(train_set)
-        report_loss, report_accuracy, start_time = [], [], time.time()
-        batch_cnt = 0
+        report_loss, report_correct, report_pred, start_time = 0, 0, 0, time.time()
+        batch_cnt, n_samples = 0, 0
         if train_instances % opt.batch_size == 0:
             total_batches = train_instances // opt.batch_size
         else:
@@ -133,20 +133,24 @@ def train():
                 gold_loc_seq = gold_loc_seq.cuda()
                 gold_state_seq = gold_state_seq.cuda()
 
-            train_loss, train_accuracy = model(char_paragraph = char_paragraph, entity_mask = entity_mask, verb_mask = verb_mask,
-                                               loc_mask = loc_mask, gold_loc_seq = gold_loc_seq, gold_state_seq = gold_state_seq)
+            train_result = model(char_paragraph = char_paragraph, entity_mask = entity_mask, verb_mask = verb_mask,
+                                 loc_mask = loc_mask, gold_loc_seq = gold_loc_seq, gold_state_seq = gold_state_seq)
+
+            train_loss, train_correct_pred, train_total_pred = train_result
 
             train_loss.backward()
             optimizer.step()
-            report_loss.append(train_loss.item())
-            report_accuracy.append(train_accuracy)
+            report_loss += train_loss.item()
+            report_correct += train_correct_pred
+            report_pred += train_total_pred
             batch_cnt += 1
+            n_samples += len(paragraphs)
 
             # time to report results
             if batch_cnt in report_batch:
 
-                output(f'{batch_cnt}/{total_batches}, Epoch {epoch_i+1}: training loss: {mean(report_loss):.3f}, '
-                      f'training state prediction accuracy: {mean(report_accuracy)*100:.3f}%, time elapse: {time.time()-start_time:.2f}')
+                output(f'{batch_cnt}/{total_batches}, Epoch {epoch_i+1}: training loss: {(report_loss / n_samples):.3f}, '
+                      f'training state prediction accuracy: {(report_correct / report_pred)*100:.3f}%, time elapse: {time.time()-start_time:.2f}')
 
                 model.eval()
                 eval_score = eval(dev_set, model)
@@ -165,10 +169,9 @@ def train():
                         output('Early Stopping!')
                         quit()
 
-                report_loss, report_accuracy, start_time = [], [], time.time()
+                report_loss, report_correct, report_pred, n_samples, start_time = 0, 0, 0, 0, time.time()
 
         epoch_i += 1
-
 
 
         # summary(model, char_paragraph, entity_mask, verb_mask, loc_mask)
@@ -180,7 +183,7 @@ def eval(dev_set, model):
     start_time = time.time()
     dev_batch = DataLoader(dataset = dev_set, batch_size = opt.batch_size, shuffle = False, collate_fn = Collate())
 
-    report_loss, report_accuracy = [], []
+    report_loss, report_correct, report_pred, n_samples = 0, 0, 0, len(dev_set)
     with torch.no_grad():
         for batch in dev_batch:
             paragraphs = batch['paragraph']
@@ -199,15 +202,18 @@ def eval(dev_set, model):
                 gold_loc_seq = gold_loc_seq.cuda()
                 gold_state_seq = gold_state_seq.cuda()
 
-            eval_loss, eval_accuracy = model(char_paragraph=char_paragraph, entity_mask=entity_mask, verb_mask=verb_mask,
+            eval_result = model(char_paragraph=char_paragraph, entity_mask=entity_mask, verb_mask=verb_mask,
                                             loc_mask=loc_mask, gold_loc_seq=gold_loc_seq, gold_state_seq=gold_state_seq)
-            report_loss.append(eval_loss.item())
-            report_accuracy.append(eval_accuracy)
 
-    output(f'Evaluation: eval loss: {mean(report_loss):.3f}, '
-          f'eval state prediction accuracy: {mean(report_accuracy)*100:.3f}%, time elapse: {time.time()-start_time:.2f}')
+            eval_loss, eval_correct_pred, eval_total_pred = eval_result
+            report_loss += eval_loss.item()
+            report_correct += eval_correct_pred
+            report_pred += eval_total_pred
 
-    return eval_accuracy
+    output(f'Evaluation: eval loss: {(report_loss / n_samples):.3f}, '
+          f'eval state prediction accuracy: {(report_correct / report_pred)*100:.3f}%, time elapse: {time.time()-start_time:.2f}')
+
+    return (report_correct / report_pred) * 100
 
 
 

utils.py
@@ -59,7 +59,7 @@ def find_allzero_rows(vector: torch.IntTensor) -> torch.BoolTensor:
     return column_sum == 0
 
 
-def compute_tag_accuracy(pred: List[List[int]], gold: List[List[int]], pad_value: int) -> float:
+def compute_tag_accuracy(pred: List[List[int]], gold: List[List[int]], pad_value: int) -> (int, int):
     """
     Given the predicted tags and gold tags, compute the prediction accuracy.
     Note that we first need to deal with the padded parts of the gold tags.
@@ -74,7 +74,7 @@ def compute_tag_accuracy(pred: List[List[int]], gold: List[List[int]], pad_value
         total_pred += len(pred[i])
         correct_pred += np.sum(np.equal(pred[i], unpad_gold[i]))
 
-    return correct_pred / total_pred
+    return correct_pred, total_pred
 
 
 def get_report_time(total_batches: int, report_times: int) -> List[int]: